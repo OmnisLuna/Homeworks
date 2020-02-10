dfa3162141a88ecb226c8aa2081aa6923ff640c2
Natalya Leonovich <nleonovich@avito.ru>
2020-02-10 12:25:41 +0300

    swift-lesson-6 homework

--- L6_nvleonovich.playground/Contents.swift
+++ L6_nvleonovich.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+class Human {
+    var years: Int
+
+    init(years: Int) {
+        self.years = years
+    }
+}
+
+struct Queue<T> {
+    private var elements: [T] = []
+    mutating func push(_ element: T) {
+        elements.append(element)
+    }
+    mutating func pop() -> T? {
+        return elements.removeFirst()
+    }
+}
+
+var human = Queue<Human>()
+//human.sort()
+human.push(Human(years:22))
+human.push(Human(years:56))
+human.push(Human(years:15))
+human.push(Human(years:38))
+human.push(Human(years:62))
+human.pop()
+human.pop()
